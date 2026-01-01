b377eab56bd16c9ff1d4a7a889cc9cde59d069fc
Maz-crypto <maz.aziz20@gmail.com>
2026-01-01 21:04:25 +0300

    Update render_build_command.sh

--- render_build_command.sh
+++ render_build_command.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-# render_build_command.sh
 
-# تثبيت تبعيات النظام المطلوبة لبناء psycopg2
-apt-get update && apt-get install -y libpq-dev gcc python3-dev
-
-# تثبيت المتطلبات
-pip install -r requirements.txt
